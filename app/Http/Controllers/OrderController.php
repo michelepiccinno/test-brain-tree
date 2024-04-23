ab4aa8d9c3a199de39b85ec8d4532035aca22f2a
michelepiccinno <michele.piccinno@gmail.com>
2024-04-23 12:01:47 +0200

    add order request

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Orders\OrderRequest;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 use Psy\Readline\Hoa\Console;
@@ -29,11 +30,11 @@ class OrderController extends Controller
         return  response()->json($data, 200);
     }
 
-    public function makePayment(Request $request, Gateway $gateway)
+    public function makePayment(OrderRequest $request, Gateway $gateway)
     {
 
         $result = $gateway->transaction()->sale([
-            'amount' => '10.00',
+            'amount' => $request->amount,
             'paymentMethodNonce' => $request->token,
             'options' => [
                 'submitForSettlement' => True
